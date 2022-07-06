62428f7cf487f78fed27177f529e664e5458e957
panchoValdes <f.valdsmartnez@uandresbello.edu>
2022-07-06 16:21:46 -0400

    diseño de login

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, TextInput, Button } from "react-native";
+import { BlurView } from 'expo-blur'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/login1.png")} style={[styles.image, StyleSheet.absoluteFill]} />
+      <ScrollView contentContainerStyle= {{
+        flex: 1,
+        width: '100%', 
+        height: '100%', 
+        alignItems: 'center',
+        justifyContent:'center'
+      }}>
+        <BlurView intensity={250}>
+          <View style={styles.login}>
+            <Image source={require("./assets/blank-profile.png")} style={styles.prefilePicture}/>
+            <View>
+              <Text style={{fontSize:17, fontWeight: '400', color:'#FF7D61'}}> E-mail </Text>
+              <TextInput style={styles.input}>E-mail</TextInput>
+            </View>
+            <View>
+              <Text style={{fontSize:17, fontWeight: '400', color:'#FF7D61'}}> Password </Text>
+              <TextInput style={styles.input} placeholder="password" secureTextEntry={true} >Password</TextInput>
+            </View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={{fontSize: 17, fontWeight:'400', color:'#FF7D61'}}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, { backgroundColor:'#B35545'}]}>
+              <Text style={{fontSize: 17, fontWeight:'400', color:'#FF7D61'}}>Create Acount</Text>
+            </TouchableOpacity>
+          </View>
+        </BlurView>
+      </ScrollView>
     </View>
   );
 }
@@ -16,5 +44,55 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  image: {
+    width:'100%', 
+    height:'100%',
+    resizeMode: 'cover',
+  },
+
+  login:{
+    width:350,
+    height:520,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center'
+
+  },
+
+  prefilePicture:{
+    width: 100,
+    height:120,
+    borderRadius:50,
+    borderColor:'#fff',
+    borderWidth:1,
+    marginVertical: 30,
+  },
+
+  input:{
+    width:250,
+    height:40,
+    borderColor:'#fff',
+    borderWidth:2,
+    borderRadius:10,
+    padding:10,
+    marginVertical:10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 20,
+  },
+
+  button:{
+    width:250,
+    height:40,
+    borderRadius:10,
+    backgroundColor:'#FFA495',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    borderColor:'#fff',
+    borderWidth: 1,
   },
 });
